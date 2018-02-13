21a3370e23c57bfe2c15cea7a3be128012e7f95f
Konstantin Sorokin <ksork@yandex.ru>
2018-02-13 20:46:28 +0500

    Задание 2 часть 1

---
+++ app/src/main/java/ru/inettel/ksork/lesson2/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.inettel.ksork.lesson2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private void showMessage(@StringRes int message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                showMessage(R.string.settings);
+                break;
+            case R.id.menu_search:
+                showMessage(R.string.search);
+                break;
+            case R.id.menu_exit:
+                showMessage(R.string.exit);
+        }
+        return true;
+    }
+}
